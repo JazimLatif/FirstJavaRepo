c863770175d3f6239ee5e24c8c7cee27a702f0a3
jazim latif <jazimlatif@github.com>
2021-10-18 12:39:00 +0100

    learning java

--- helloWorld/src/main/java/Main.java
+++ helloWorld/src/main/java/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hola");
+        String name = "Jazim";
+        int age = 21;
+        long age2 = 23L;
+
+        double pi = 3.14;
+        float pi2 = 3.14159615F;
+
+        char letter = 'G';
     }
 }
